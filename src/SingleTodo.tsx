4be7c5f52e0d4c5191f0132232fd06da309cd94d
Wesley Sum <77678835+sumtzehern@users.noreply.github.com>
2023-10-01 16:56:21 -0400

    added singletodolist and useReducer

--- src/SingleTodo.tsx
+++ src/SingleTodo.tsx
@@ -1,32 +1,77 @@
-import { type } from 'os'
-import React from 'react'
-import { Todo } from './model'
-import {AiFillEdit, AiFillDelete } from "react-icons/ai"
-import {MdDone } from "react-icons/md"
+import { type } from "os";
+import React, { useState } from "react";
+import { Todo } from "./model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./style.css";
+import TodoList from "./TodoList";
 
 type Props = {
-    todo:Todo,
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-const SingleTodo = ({todo,todos,setTodos}:Props) => {
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id:number ) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? {...todo, todo:editTodo} : todo))
+    )
+    setEdit(false);
+  }
+
+
+
   return (
-    <form className='todos_single'>
-      <span className='todos_single--text'>
-        {todo.todo}
-      </span>
-      <div className='icon'>
-        <AiFillEdit />
-      </div>
-      <div className='icon'>
-        <AiFillDelete/>
-      </div>
-      <div className='icon'>
-        <MdDone/>
+    <form className="todos_single" onSubmit={(e)=> handleEdit(e,todo.id)}>
+      {edit ? (
+        <input
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos_single--test"
+        />
+      ) : todo.isDone ? (
+        <s className="todos_single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single--text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,26 +1,50 @@
-import React from 'react'               
+import React from "react";
 import "./style.css";
-import { Todo } from './model';
-import SingleTodo from './SingleTodo';
+import { Todo } from "./model";
+import SingleTodo from "./SingleTodo";
 
-interface Props{
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-};
- 
-const TodoList: React.FC<Props>= ({todos,setTodos}:Props) => {
-  return (
-    <div className='todos'>
-        {
-        todos.map(todo => (
-          <SingleTodo todo={todo} key={todo.id} 
-          todos={todos}
-          setTodos={setTodos}
-          />
-        ))
-    }
-    </div>
-  )
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
+
+const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
+  return (
+    // <div className="todos">
+    //   {todos.map((todo) => (
+    //     <SingleTodo
+    //       todo={todo}
+    //       key={todo.id}
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //     />
+    //   ))}
+    // </div>
+    <div className="container">
+      <div className="todos">
+        <span className="todos_heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos_heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
